bee600992287b9b5dc3e5816f0fb6b7d91bdf21d
Nelson Ghezzi <nelsonghezzi@gmail.com>
2019-07-27 21:03:26 -0300

    Chapter 12 - The First Unit Test (refactor)
    Refactor to use JUnitRuleMockery with JUnit @Rule instead of the
    deprecated @RunWith annotation with the JMock class.

--- src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -3,17 +3,16 @@ package test.auctionsniper;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
 import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import auctionsniper.AuctionEventListener;
 import auctionsniper.AuctionMessageTranslator;
 
-@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
-  private final Mockery context = new Mockery();
+  @Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
+
   private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
   private final AuctionMessageTranslator translator = new AuctionMessageTranslator(listener);
 
